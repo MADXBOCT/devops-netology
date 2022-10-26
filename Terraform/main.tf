a196b4f15cddb06a5fad6410d91dd905f746737b
Sergey Gudimov <passive_permit_0e@icloud.com>
2022-10-26 11:28:46 +0300

    07-terraform-02-syntax p1-2

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -16,7 +16,7 @@ resource "yandex_vpc_network" "default" {
 
 resource "yandex_vpc_subnet" "default" {
   name = "subnet"
-  network_id     = "${yandex_vpc_network.default.id}"
+  network_id     = yandex_vpc_network.default.id
   v4_cidr_blocks = ["192.168.101.0/24"]
 }
 
--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,9 +1,15 @@
+output "ya_cloud_id" {
+  value = yandex_compute_instance.my-tf-server1.
+}
 
+output "zone" {
+  value = yandex_compute_instance.my-tf-server1.zone
+}
 
 output "external_ip_address_my-tf-server1" {
-  value = "${yandex_compute_instance.my-tf-server1.network_interface.0.nat_ip_address}"
+  value = yandex_compute_instance.my-tf-server1.network_interface[0].nat_ip_address
 }
 
 output "subnet_id" {
-    value = "${yandex_vpc_network.default.id}"
+    value = yandex_vpc_network.default.id
 }
